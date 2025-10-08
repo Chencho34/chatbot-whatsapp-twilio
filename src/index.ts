bd53e5037baacaaf1b3eed9c006e3e37d8c22738
Chencho34 <armandocrescencio343@gmail.com>
2025-10-07 19:01:36 -0600

    initial commit

--- src/index.ts
+++ src/index.ts
@@ -1,81 +1,41 @@
 import bodyParser from 'body-parser'
-import axios from 'axios'
-
+import express, { 
+  type Request,
+  type Response
+} from 'express'
 
 const app = express()
+
 app.use(bodyParser.urlencoded({ extended: false }))
 
-const TWILIO_ACCOUNT_SID = process.env.TWILIO_SID!
-const TWILIO_AUTH_TOKEN = process.env.TWILIO_TOKEN!
-const FROM = 'whatsapp:+14155238886'
-const TO = 'whatsapp:+522721305556'
+app.get('/', (_, res) => {
+  res.send('Hello world!')
+})
 
-app.get('/', (_, res) => res.send('Hello world!'))
+app.post('/whatsapp', (req: Request, res: Response) => {
+  const message = req.body.Body
+  // const from = req.body.From    
 
-// app.post('/whatsapp', async (req: Request, res: Response) => {
-//   const message = (req.body.Body || '').toLowerCase()
-//   let reply = 'No entendí tu mensaje 🤔'
+  let reply = 'No entendí tu mensaje 🤔'
 
-//   if (message.includes('productos')) {
-//     reply = 'Hola soy yami en que puedo ayudarte'
-//   }
+  if (message.toLowerCase().includes('productos')) {
+    reply = 'Hola aqui te muestro algunos productos'
+  }
 
-//   // Enviar un template de Twilio (opcional)
-//   // const TO = req.body.From // enviar respuesta al que escribió
-//   console.log(TO)
-  const url = `https://api.twilio.com/2010-04-01/Accounts/${TWILIO_ACCOUNT_SID}/Messages.json`
+  if (message.toLowerCase().includes('hola')) {
+    reply = '¡Hola! Soy tu chatbot de prueba 🚀 hola yami como estas'
+  }
 
-//   const data = new URLSearchParams({
-//     From: FROM,
-//     To: TO,
-//     ContentSid: 'HX668f7abd624c62d3b80f112dae2af749'
-//   })
-
-//   try {
-//     const result = await axios.post(url, data, {
-//       auth: {
-//         username: TWILIO_ACCOUNT_SID,
-//         password: TWILIO_AUTH_TOKEN
-//       }
-//     })
-//      console.log('Twilio response:', result.data)
-//   } catch (err) {
-//     console.error('Error enviando template:', err)
-//   }
-
-//   res.set('Content-Type', 'text/xml')
-//   res.send(`
-//     <Response>
-//       <Message>${reply}</Message>
-//     </Response>
-//   `)
-// })
-
-const hola =' hola'
-
-app.post('/whatsapp', async (req: Request, res: Response) => {
-  const TO = req.body.From // responde al que envió el mensaje
-  const data = new URLSearchParams({
-    From: FROM,
-    To: TO,
-    ContentSid: 'HX0052b586690e45519d8dbd2d4f51fa51'
-  })
-
-  try {
-    const result = await axios.post(url, data, {
-      auth: {
-        username: TWILIO_ACCOUNT_SID!,
-        password: TWILIO_AUTH_TOKEN!
-      }
-    })
-    console.log('Twilio response:', result.data)
-  } catch (err: any) {
-    console.error('Error enviando template:', err.response?.data || err.message)
+  if (message.toLowerCase().includes('telefonos')) {
+    reply = 'Aquí te recomendamos algunos teléfonos que pueden interesarte'
   }
 
   res.set('Content-Type', 'text/xml')
-  res.send('<Response></Response>')
+  res.send(`
+    <Response>
+      <Message>${reply}</Message>
+    </Response>
+  `)
 })
-
 
 app.listen(3000, () => console.log('Bot corriendo en http://localhost:3000'))
